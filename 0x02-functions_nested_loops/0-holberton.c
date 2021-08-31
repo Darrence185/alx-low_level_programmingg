a84bf455e5ec553c31ca45f4fd4ee3e26d866afd
Chrissjoejo <Chrissjoejobrown112@gmail.com>
2021-08-31 21:53:05 +0100

    0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -4,16 +4,16 @@
  *  * main - prints Holberton as a message
  *   *
  *    * Return: Always 0 (Success)
- *   
- *  */
+ *     */
 int main(void)
 {
-   int i = 0;
-   char s[] = "_putchar\n";
+        int i = 0;
+            char s[] = "_putchar\n";
 
-   while (s[i])
-   _putchar (s[i]);
-   i++;
-   }
-    return (0);
+                while (s[i])
+                        {
+                                    _putchar (s[i]);
+                                            i++;
+                                                }
+                    return (0);
 }

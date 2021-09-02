5acb6ddf2bb201e36b123ed27bba8245d26ef6c1
Chrissjoejo <Chrissjoejobrown112@gmail.com>
2021-09-02 01:02:08 +0100

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - Prints the absolute value of an integer
+ *
+ * @a: Input value to check
+ *
+ * Return: 0 or 1
+ */
+
+int _abs(int a)
+{
+	return (a >= 0 ? a : a * -1);
+}

cb680a26876bd014dfd8a273b8921d3aa9826935
Chrissjoejo <Chrissjoejobrown112@gmail.com>
2021-09-06 18:46:46 +0100

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strlen - swap parameter values.
+ *
+ * @s: pointer to a char.
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int counter = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		counter++;
+	}
+	return (counter);
+}

ba0f296b233d287f60a3cf5f66e5936cc0ca8211
Chrissjoejo <Chrissjoejobrown112@gmail.com>
2021-09-06 19:34:25 +0100

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,35 +1,20 @@
+#include "holberton.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
- * main - program that generates random valid
- * passwords for the program 101-crackme
+ * main - check the code for ALX School students.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
-	int pass[100];
-	int i, sum, n;
+    int a;
+    int b;
 
-	sum = 0;	
-
-	srand(time(NULL));
-
-	for (i = 0; i < 100; i++)
-	{
-		pass[i] = rand() % 78;
-		sum += (pass[i] + '0');
-		putchar(pass[i] + '0');
-		if ((2772 - sum) - '0' < 78)
-		{
-			n = 2772 - sum - '0';
-			sum += n;
-			putchar(n + '0');
-			break;
-		}
-	}
-
-	return (0);
+    a = 98;
+    b = 42;
+    printf("a=%d, b=%d\n", a, b);
+    swap_int(&a, &b);
+    printf("a=%d, b=%d\n", a, b);
+    return (0);
 }

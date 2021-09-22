c3d82d4c8a42ddba78ceb51e4874f5cfb7089372
Chrissjoejo <Chrissjoejobrown112@gmail.com>
2021-09-22 00:57:19 +0000

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * *_strpbrk - search a string for any of a set of bytes.
+ *
+ * @s: string to search.
+ * @accept: string containing bytes to look for
+ *
+ * Return: pointer to the byte in s == accept or NULL
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; *s != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (*s == accept[j])
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+
+	return (0);
+}

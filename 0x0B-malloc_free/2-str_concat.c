446dbcc46f063815b2f84361ead98b74a22d8d5e
Chrissjoejo <Chrissjoejobrown112@gmail.com>
2021-09-23 02:29:31 +0000

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,65 +1,77 @@
+#include "main.h"
 #include <stdlib.h>
-#include "holberton.h"
-int _strlen(char *s);
+#include <stdio.h>
 
 /**
- * str_concat -  returns a pointer to a newly allocated space in memory
- * which contains the string value of concatenation of s2 upon s1.
- * @s1: first string.
- * @s2: second string.
- * Return: pointer to array of char.
+ * _strlen - find length of a string
+ *
+ * @s: string
+ *
+ * Return: int
  */
-char *str_concat(char *s1, char *s2)
-{
-	int lens1, lens2;
-	int i, k;
-	char *arr;
 
-	lens1 = _strlen(s1);
-	lens2 = _strlen(s2);
-	arr = malloc(sizeof(char) * (lens1 + lens2 + 1));
-	if (arr == NULL)
-	{
-		return (NULL);
-	}
-	else
-	{
-		if (s1)
-		{
-			for (i = 0; i < lens1; i++)
-			{
-				arr[i] = s1[i];
-			}
-		}
-		if (s2)
-		{
-			k = 0;
-			while (k < lens2)
-			{
-				arr[i] = s2[k];
-				i++;
-				k++;
-			}
-		}
-		arr[i] = '\0';
-		return (arr);
-	}
-}
-/**
- * _strlen - compute string length.
- *
- * @s: pointer to a string.
- *
- * Return: length of string
- */
+
 int _strlen(char *s)
 {
-	int counter = 0;
+	int size = 0;
 
-	while (*s != '\0')
+	for (; s[size] != '\0'; size++)
 	{
-		s++;
-		counter++;
+		;
 	}
-	return (counter);
+
+	return (size);
+}
+
+/**
+ * *str_concat - concatenates two strings
+ *
+ * @s1: string 1
+ *
+ * @s2: string 2
+ *
+ * Return: pointer
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int size1, size2, i;
+	char *m;
+
+	if (s1 == NULL)
+	{
+		s1 = "\0";
+	}
+
+	if (s2 == NULL)
+	{
+		s2 = "\0";
+	}
+
+	size1 = _strlen(s1);
+	size2 = _strlen(s2);
+
+	m = malloc((size1 + size2) * sizeof(char) + 1);
+
+	if (m == 0)
+	{
+		return (0);
+	}
+
+	for (i = 0; i <= size1 + size2; i++)
+	{
+		if (i < size1)
+		{
+			m[i] = s1[i];
+		}
+
+		else
+		{
+			m[i] = s2[i - size1];
+		}
+	}
+
+	m[i] = '\0';
+
+	return (m);
 }

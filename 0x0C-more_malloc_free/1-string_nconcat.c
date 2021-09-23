075919baef45b82c7d2df959f075759371a8000f
Chrissjoejo <Chrissjoejobrown112@gmail.com>
2021-09-23 12:15:15 +0000

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,49 +1,52 @@
-#include "holberton.h"
 #include <stdlib.h>
-
+#include "holberton.h"
+int _strlen(char *s);
 /**
- * string_nconcat - concatenates two strings.
+ * string_nconcat - concatenate two strings.
  * @s1: first string.
  * @s2: second string.
- * @n: amount of bytes.
- *
- * Return: pointer to the allocated memory.
- * if malloc fails, status value is equal to 98.
+ * @n: num of bytes to be apended from s2.
+ * Return: pointer to allocated memory containing s1 folowed by nbytes of s2.
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *sout;
-	unsigned int ls1, ls2, lsout, i;
+	char *str;
+	unsigned int i, j, lens1 = 0, lens2 = 0;
 
 	if (s1 == NULL)
 		s1 = "";
-
 	if (s2 == NULL)
 		s2 = "";
-
-	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
-		;
-
-	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
-		;
-
-	if (n > ls2)
-		n = ls2;
-
-	lsout = ls1 + n;
-
-	sout = malloc(lsout + 1);
-
-	if (sout == NULL)
+	lens1 = ((unsigned int)_strlen(s1));
+	lens2 = ((unsigned int)_strlen(s2));
+	if (n < lens2)
+		lens2 = n;
+	str = malloc(sizeof(char) * (lens1 + lens2 + 1));
+	if (str == NULL)
 		return (NULL);
+	for (i = 0; i < lens1; i++)
+		str[i] = s1[i];
+	for (j = 0; j < lens2; j++)
+	{
+		str[i] = s2[j];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+/**
+ * _strlen - get string length.
+ * @s: pointer to a char.
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int counter = 0;
 
-	for (i = 0; i < lsout; i++)
-		if (i < ls1)
-			sout[i] = s1[i];
-		else
-			sout[i] = s2[i - ls1];
-
-	sout[i] = '\0';
-
-	return (sout);
+	while (*s != '\0')
+	{
+		s++;
+		counter++;
+	}
+	return (counter);
 }

4779dbd3c293a6b3755c31f27b2b875a7b59a2e8
drwamunga <87418352+drwamunga@users.noreply.github.com>
2022-04-01 03:40:10 +0300

    search algo

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,42 +1,51 @@
 #include "search_algos.h"
 
 /**
- * binary_search - searches for a value in a sorted array
- * @array: arr of ints
- * @size: size of arr
- * @value: value to find in arr
- * Return: print or -1 if fail
-*/
+ * print_arr - helper func to print array everytime array is halved
+ * @array: array
+ * @l: left index of original array
+ * @r: right index of original array
+ */
+void print_arr(int *array, size_t l, size_t r)
+{
+	size_t i = 0;
+
+	printf("Searching in array: ");
+	for (i = l; i <= r; i++)
+	{
+		if (i != r)
+			printf("%d, ", array[i]);
+		else
+			printf("%d\n", array[i]);
+	}
+}
+
+/**
+ * binary_search - search mids
+ * @array: given array of ints
+ * @size: size of array
+ * @value: value to search for
+ * Return: index at which value's found
+ */
 int binary_search(int *array, size_t size, int value)
 {
-	int first = 0, last = 0, middle = 0, idx = 0;
+	size_t mid = 0;
+	size_t l = 0;
+	size_t r = size - 1;
 
-	if (array == NULL || size == 0)
+	if (!array)
 		return (-1);
 
-	last = size - 1;
-
-	while (first <= last)
+	while (l <= r)
 	{
-		idx = 0;
-		printf("Searching in array: ");
-		for (idx = first; idx <= last; idx++)
-			if (idx == last)
-			{
-				printf("%d\n", array[last]);
-				break;
-			}
-			else
-				printf("%d, ", array[idx]);
-
-		middle = (first + last) / 2;
-		if (array[middle] < value)
-			first = middle + 1;
-		else if (array[middle] > value)
-			last = middle - 1;
+		print_arr(array, l, r);
+		mid = (l + r) / 2;
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] > value)
+			r = mid - 1;
 		else
-			return (middle);
+			l = mid + 1;
 	}
-
 	return (-1);
 }
